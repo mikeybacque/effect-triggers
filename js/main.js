f2df3a6202d67dc9e78074d9f1dca77c7eb33b38
mikeybacque <mikey.bacque@gmail.com>
2016-03-29 17:26:14 -0400

    first 4 animations

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$('.btn-show-hide').on('click', function (e) {
+  $('.box').toggleClass('is-clicked');
+});
+
+$('.btn-move').on('click', function (e) {
+  $('.diamond').toggleClass('is-clicked2');
+});
+
+$('.btn-collapse-expand').on('click', function (e) {
+  $('.panel').toggleClass('is-clicked3');
+});
+
+$('.btn-bounce').on('click', function (e) {
+  $('.circle').addClass('is-clicked4');
+});
+
+$('.circle').on('animationend', function (e) {
+  $(this).removeClass('is-clicked4');
+});
